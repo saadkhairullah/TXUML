2d42ef854fa30324fa015b89141cc7f9d18b2828
saadkhairullah <sxk7397@mavs.uta.edu>
2025-07-12 21:16:04 -0500

    added login and sign in

---
+++ tx-uml/app/signup/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const signup
+ = () => {
+    // signup page lives inside the two divs
+  return (
+    <div>
+        signup
+    </div>
+  )
+}
+
+export default signup
