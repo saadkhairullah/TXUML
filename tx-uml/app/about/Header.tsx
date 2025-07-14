35ce748c6e3265a6a12259e2ee07f17cfda2550e
Eduardo Barrera <barreraedward28@gmail.com>
2025-07-14 12:07:44 -0500

    landing page preparations

---
+++ tx-uml/app/about/Header.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header: React.FC = () => (
+  <header style={{ padding: '1rem', backgroundColor: '#282c34', color: 'white' }}>
+    <h1>TXUML</h1>
+  </header>
+);
+
+export default Header;
