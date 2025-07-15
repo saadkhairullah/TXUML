331aa4df95941d549535e08270684c16558790c5
pik8014 <pierre.konan@mavs.uta.edu>
2025-07-15 14:20:09 -0500

    fix class error

--- tx-uml/app/login/page.tsx
+++ tx-uml/app/login/page.tsx
@@ -1,24 +1,24 @@
 import React from 'react'
-
+import './style.css'
 const login = () => {
     //  pages"login page lives inside the two divs
   return (
     <div>
-      <h1>Welcome to <span class="logo">MineApp</span></h1>
-    <form action="/submit" class="join-form">
+      <h1>Welcome to <span className="logo">MineApp</span></h1>
+    <form action="/submit" className="join-form">
       <h2><span>Log in </span>to your account</h2>
-      <div class="input-group">
+      <div className="input-group">
         <label for="">Email</label>
         <input type="text" />
       </div>
-      <div class="input-group">
+      <div className="input-group">
         <label for="">Password</label>
         <input type="password" />
       </div>
-      <div class="input-group">
-        <button type="submit" class="btn">Sign in</button>
+      <div className="input-group">
+        <button type="submit" className="btn">Sign in</button>
       </div>
-      <a href="" class="register">New to ? Create your account.</a>
+      <a href="" className="register">New to ? Create your account.</a>
     </form>  
     </div>
   )
