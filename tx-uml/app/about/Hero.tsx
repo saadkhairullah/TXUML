432ac5f5ae4263c2e4f7eda068744f59ad8c185a
Eduardo Barrera <barreraedward28@gmail.com>
2025-07-15 14:05:13 -0500

    page tweaks

--- tx-uml/app/about/Hero.tsx
+++ tx-uml/app/about/Hero.tsx
@@ -3,19 +3,23 @@ import Image from 'next/image';
 import styles from './Hero.module.css';
 
 const Hero: React.FC = () => (
-  <section style={{ padding: '4rem', textAlign: 'center' }}>
-    <h2 className={styles.heading}>TXUML testing text</h2>
-    <p className={styles.subtext}>TXUML is an interactive map that highlights the locations 
-        of all underground abandoned or active coal mines in the 
-        state of Texas since the late 1800s.
-    </p>
+  <section className={styles.heroSection}>
+    <div className={styles.textContent}>
+      <h2 className={styles.heading}>TXUML testing text</h2>
+      <p className={styles.subtext}>TXUML is an interactive map that highlights the locations 
+          of all underground abandoned or active coal mines in the 
+          state of Texas since the late 1800s.
+      </p>
+    </div>
+    <div>
     <Image
-            src={"/images/tunnel1.png"}
-            alt="Underground Mine Tunnel"
-             width={240}
-            height={320}
-            style={{ borderRadius: '4px' }}
-            />
+        src={"/images/tunnel1.png"}
+        alt="Underground Mine Tunnel"
+          width={420}
+        height={480}
+        style={{ borderRadius: '4px' }}
+        />
+    </div>
   </section>
 );
 
