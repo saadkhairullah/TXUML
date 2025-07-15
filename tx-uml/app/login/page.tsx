b66be3fd4a8c08609bdcbdb65435e624aaf78d9c
pik8014 <pierre.konan@mavs.uta.edu>
2025-07-15 14:00:59 -0500

    login and signup page

--- tx-uml/app/login/page.tsx
+++ tx-uml/app/login/page.tsx
@@ -4,7 +4,22 @@ const login = () => {
     //  pages"login page lives inside the two divs
   return (
     <div>
-      loginpage  
+      <h1>Welcome to <span class="logo">MineApp</span></h1>
+    <form action="/submit" class="join-form">
+      <h2><span>Log in </span>to your account</h2>
+      <div class="input-group">
+        <label for="">Email</label>
+        <input type="text" />
+      </div>
+      <div class="input-group">
+        <label for="">Password</label>
+        <input type="password" />
+      </div>
+      <div class="input-group">
+        <button type="submit" class="btn">Sign in</button>
+      </div>
+      <a href="" class="register">New to ? Create your account.</a>
+    </form>  
     </div>
   )
 }
--- tx-uml/app/signup/page.tsx
+++ tx-uml/app/signup/page.tsx
@@ -5,7 +5,25 @@ const signup
     // signup page lives inside the two divs
   return (
     <div>
-        signup
+        <h1>Welcome to <span class="logo">MineApp</span></h1>
+    <form action="/submit" class="join-form">
+      <h2><span>Join us </span>to get started</h2>
+      <div class="input-group">
+        <label for="">Full name</label>
+        <input type="text" />
+      </div>
+      <div class="input-group">
+        <label for="">Email</label>
+        <input type="text" />
+      </div>
+      <div class="input-group">
+        <label for="">Password</label>
+        <input type="password" />
+      </div>
+      <div class="input-group">
+        <button type="submit" class="btn">Join now</button>
+      </div>
+      <a href="" class="register">already have an account ? Sign in.</a>
     </div>
   )
 }
