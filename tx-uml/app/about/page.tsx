eea55d4c5ea60c0a46d7748a219c6edd7e60d7c3
pik8014 <pierre.konan@mavs.uta.edu>
2025-07-19 19:20:18 -0500

    update about page

--- tx-uml/app/about/page.tsx
+++ tx-uml/app/about/page.tsx
@@ -1,41 +1,60 @@
 import React from "react";
-import Image from "next/image";
-import styles from "./page.module.css";
-// Use a public URL for the video source instead of importing the mp4 file
-// import introBg from "./assets/introBg.mp4"; 
-
-
+import ImageLogo from "../assets/images/TXUML-logo.svg";
+import Image from "../assets/images/laptop-img.svg";
+import "./page.css";
+import "../index.css"; // Assuming you have a global CSS file for styles
 
 // this is where the about page frontend goes
+const Video = () => {
+  return (
+    <video controls autoPlay muted loop>
+      <source src="../assets/introBg.webm" type="video/webm" />
+      <source src="../assets/introBg.mp4" type="video/mp4" />
+    </video>
+  );
+};
+
 const AboutPage = () => {
   return (
     <div>
+      <nav className="navbar">
+        <a href="" className="logo-name">
+          <img src={ImageLogo.src} alt="TXUML Logo" />
+          <h3>TXUML</h3>
+        </a>
+        <div className="nav-links">
+          <a href="">Log in</a>
+          <a href="">Sign up</a>
+        </div>
+      </nav>
+
       <div>
-        <nav className="navbar">
-          <a href="" className="logo-name">
-            <img src={/images/trace.svg} />
-            <h3>TXUML</h3>
-          </a>
-          <div className="nav-links">
-            <ul>
-              <li>
-                <a href="">Log in</a>
-              </li>
-              <li>
-                <a href="">Sign up</a>
-              </li>
-            </ul>
+        <div className="hero-section">
+          <Video />
+          <div className="hero-content">
+            <h1>
+              Unearth the Data
+              <br /> Build with Confidence.
+            </h1>
+            <p>Every Mine Has a Story. We Map Them All, from Depths to Data.</p>
+            <a href="">Get started</a>
           </div>
-        </nav>
-      </div>
-      <div>
-        <video src="/videos/introBg.mp4" controls autoPlay muted loop />
-        {/* <video src={introBg}controls autoPlay muted loop /> */}
-      
-        <h1>Unearth the Data. Build with Confidence.</h1>
-        <p>
-         Every Mine Has a Story. We Map Them All, from Depths to Data.
-        </p>
+        </div>
+
+        <div className="about-section">
+          <h2>About Us</h2>
+          <div className="about-content">
+            <p>
+              TXUML is an interactive map that highlights the locations of all
+              underground abandoned or active coal mines in the state of Texas
+              since the late 1800s.
+            </p>
+            <div className="image-container">
+          <img src={Image.src} alt="TXUML Logo" />
+          </div>
+          </div>
+        <h2>Frequently Asked Questions</h2>
+        </div>
       </div>
     </div>
 
