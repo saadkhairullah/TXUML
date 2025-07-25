f5bb9ca80ea440e12955bdf694469480e07cc740
saadkhairullah <sxk7397@mavs.uta.edu>
2025-07-25 17:59:52 -0500

    polished some UI that got messed up earlier

--- tx-uml/app/page.tsx
+++ tx-uml/app/page.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Hero from "./components/landing-page/hero";
 import About from "./components/landing-page/About";
+import "./page.css"; // Import page-specific styles
 
 // Use the video file as a static asset by referencing its path directly in the video tag
 import "./page.css";
@@ -9,9 +10,9 @@ import "./page.css";
 
 const AboutPage = () => {
   return (
-    <div>
+    <div className="wrapper">
       <Hero />
-      <About />       
+      <About />
     </div>
   );
 };
