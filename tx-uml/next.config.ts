ce0a6d06778b598eee5629d59ae3c20e21e9b575
saadkhairullah <144730913+saadkhairullah@users.noreply.github.com>
2025-08-06 00:02:11 -0500

    Update next.config.ts

--- tx-uml/next.config.ts
+++ tx-uml/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    eslint: {
+ typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
     ignoreDuringBuilds: true,
   },
 };
